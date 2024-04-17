2e03f88af9ac2c8203ff5138f1418bc11050f7e3
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-04-17 17:40:17 -0400

    Improve hunters story for llm

--- src/storyteller.py
+++ src/storyteller.py
@@ -4,11 +4,12 @@ import google.generativeai as genai
 from IPython.display import Markdown
 
 from src.deserialization import decision_matrices
+from src.players import Player
 
 
 class GeminiClient:
     def __init__(self):
-        API_KEY = 'AIzaSyD4oPew_mhZ4igkQ_3QXFtupQZ_ZVavg74'
+        API_KEY = 'AIzaSyBdMrVOCjzemRTjvM_BMWuyf6iZRbisWUc'
         genai.configure(api_key=API_KEY)
         self.model = genai.GenerativeModel('gemini-pro')
 
@@ -25,19 +26,21 @@ class StoryTeller:
         text = text.replace('•', '  *')
         return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-    def tell_round_story(self, title, player1, player2, action1, action2, index=0, description='historia breve'):
+    def tell_round_story(self, title, player1: Player, player2: Player, action1, action2, index=0, description='historia breve'):
         decision_matrix = decision_matrices[title]
         action_player_1 = decision_matrix.actions[action1]
         action_player_2 = decision_matrix.actions[action2]
+        player1_name = player1.name
+        player2_name = player2.name
 
         story = f"A partir del siguiente cuento"
         story += f"\nCuento: {decision_matrix.story}"
 
         story += f"\n\nRedacta un/una {description} en el que describas fielmente las decisiones tomadas por cada personaje que se listan a continuación"
-        story += f"\n{player1}: '{action_player_1}'."
-        story += f"\n{player2}: '{action_player_2}'."
+        story += f"\n{player1_name}: '{action_player_1}'."
+        story += f"\n{player2_name}: '{action_player_2}'."
 
-        story += f"\n\nTen en cuenta que {player1} y {player2} son personas. Además ambos toman la decisión sin conocer la decisión del otro."
+        story += f"\n\nTen en cuenta que {player1_name} y {player2_name} son personas. Además ambos toman la decisión sin conocer la decisión del otro."
 
         response = self.client.generate_content(story)
 
